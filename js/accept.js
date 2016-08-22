3559805e886ad6c7d6f955e75a0663c102ee7a4d
patlockley <patrick.lockley@googlemail.com>
2016-08-22 13:40:07 +0100

    New JS function

---
+++ js/accept.js
@@ -0,0 +1,12 @@
+function fewster_accept_scan(){
+	
+	var data = {
+		action : "fewster_accept_scan",
+		nonce : fewster_accept.nonce
+	};
+		
+	jQuery.post(fewster_accept.ajaxURL, data, function(response) {
+			alert(response);	
+		}
+	);
+}
