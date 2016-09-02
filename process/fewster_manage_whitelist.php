ad676034f50b77cd7e76a1430cb1f5cbd31cb152
patlockley <patrick.lockley@googlemail.com>
2016-09-03 00:44:07 +0100

    Whitelisting

---
+++ process/fewster_manage_whitelist.php
@@ -0,0 +1,59 @@
+<?PHP
+
+	class fewster_manage_whitelist{
+	
+		function __construct(){
+			add_action("admin_menu", array($this, "menu_create"));
+			add_action('network_admin_menu', array($this, 'menu_create'));
+		}
+	
+		function menu_create(){
+			add_submenu_page( "fewster-anti-bad", __("Manage Whitelist"), __("Manage Whitelist"), "manage_options", "fewster-manage-whitelist", array($this, "whitelist") );
+		}
+		
+		function whitelist($file){
+			?><h2><?PHP echo __("Whitelistied files"); ?></h2><?PHP
+			
+			if(isset($_POST['fewster_file'])){
+	
+				if(wp_verify_nonce($_POST['fewster_repair'],"fewster_repair")){
+				
+					$whitelist = get_option("fewster_whitelist");
+				
+					$output = "";
+				
+					foreach($_POST as $key => $value){
+						if($value == "remove"){
+							if(($index = array_search(str_replace("_php",".php",$key), $whitelist)) !== false) {
+								unset($whitelist[$index]);
+								$output .= "<p>" . str_replace("_php",".php",$key) . " " . __("removed from whitelist") . "</p>";
+							}
+						}
+					}
+					if($output!=""){
+						?><div class="notice error"><?PHP echo $output; ?></div><?PHP
+					}
+					update_option("fewster_whitelist", $whitelist);
+					
+				}
+	
+			}
+			?>
+			<p><?PHP echo __("These files are in the whitelist. Tick the checkbox to remove them"); ?>?</p>
+			<form action="" method="POST">
+				<input type="hidden" name="fewster_file" value="fewster_file" />
+				<?PHP
+					$whitelist = get_option("fewster_whitelist");
+					foreach($whitelist as $index => $file){
+						?><p><input type="checkbox" name="<?PHP echo $file; ?>" value="remove" /><?PHP echo $file . "</p>";
+					}
+				?>
+				<input type="submit" class="button-primary" value="<?php _e('Remove from Whitelist') ?>" />
+				<?PHP echo wp_nonce_field("fewster_repair","fewster_repair"); ?>
+			</form>
+			<?PHP
+		}
+		
+	}
+	
+	$fewster_manage_whitelist = new fewster_manage_whitelist();
---
+++ process/fewster_whitelist.php
@@ -0,0 +1,46 @@
+<?PHP
+
+	class fewster_whitelist{
+	
+		function __construct(){
+			add_action("admin_menu", array($this, "menu_create"));
+			add_action('network_admin_menu', array($this, 'menu_create'));
+		}
+	
+		function menu_create(){
+			add_submenu_page( "fewster-anti-bad", __("Add"), __("Add"), "manage_options", "fewster-whitelist", array($this, "whitelist") );
+		}
+		
+		function whitelist($file){
+			?><h2><?PHP echo __("Whitelisting"); ?> <?PHP echo $_GET['file']; ?></h2><?PHP
+			if(!isset($_POST['fewster_file'])){
+				?>
+				<p><?PHP echo __("Are you certain you wish to whitelist") . " " . $_GET['file']; ?>?</p>
+				<form action="" method="POST"> 
+					<input type="hidden" name="fewster_file" value="<?PHP echo $_GET['file']; ?>" />
+					<input type="submit" class="button-primary" value="<?php _e('Whitelist') ?>" />
+					<?PHP echo wp_nonce_field("fewster_repair","fewster_repair"); ?>
+				</form>
+				<?PHP
+			}else{
+				if(wp_verify_nonce($_POST['fewster_repair'],"fewster_repair")){
+				
+					$whitelist = get_option("fewster_whitelist");
+					
+					if(!is_array($whitelist)){
+						$whitelist = array();
+					}
+				
+					array_push($whitelist, $_POST['fewster_file']); 
+				
+					update_option("fewster_whitelist", $whitelist);
+					
+					echo $_POST['fewster_file'] . " " . __("has been whitelisted");
+					
+				}
+			}
+		}
+		
+	}
+	
+	$fewster_whitelist = new fewster_whitelist();
